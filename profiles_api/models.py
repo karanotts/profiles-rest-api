41ba4d2bbc214ba5e7a2a916ae8162d49b05f662
Kara Nottingham <kara.nottingham@eigentech.com>
2020-04-24 14:27:07 +0100

    enabled django admin

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -29,7 +29,7 @@ class UserProfileManager(BaseUserManager):
         user = self.create_user(email, name, password)
 
         user.is_superuser = True
-        user.is_admin = True
+        user.is_staff = True
 
         user.save(using=self._db)
 
@@ -41,7 +41,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
 
     objects = UserProfileManager()
 
